f4c07eb49fa92021a42c28fdb6e7c06e395000bf
TAeE <98029204+Haemyon@users.noreply.github.com>
2023-08-23 14:14:24 +0900

    0823

--- Source/LOG/00_Character/LOGPlayerController.cpp
+++ Source/LOG/00_Character/LOGPlayerController.cpp
@@ -7,11 +7,15 @@ void ALOGPlayerController::OnPossess(APawn* aPawn)
 {
 	Super::OnPossess(aPawn);
 
+	UE_LOG(LogTemp, Warning, TEXT("OnPossess"));
+
 	if (MainWidgetObject != nullptr)
 	{
+		UE_LOG(LogTemp, Warning, TEXT("Set MainWidget"));
 		MainWidget = CreateWidget<UMainWidget>(this, MainWidgetObject);
 		if (MainWidget != nullptr)
 		{
+			UE_LOG(LogTemp, Warning, TEXT("MainWidget is AddToViewport"));
 			MainWidget->AddToViewport();
 		}
 		else {
