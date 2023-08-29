e778c52cdd1ede534b7c556460296f1f62860598
TAeE <98029204+Haemyon@users.noreply.github.com>
2023-08-29 15:57:03 +0900

    0829

--- Source/LOG/00_Character/LOGPlayerController.cpp
+++ Source/LOG/00_Character/LOGPlayerController.cpp
@@ -44,30 +44,30 @@ void ALOGPlayerController::OnPossess(APawn* aPawn)
 		UE_LOG(LogTemp, Warning, TEXT("MainWidgetObject == nullptr"));
 	}
 
-	static ConstructorHelpers::FObjectFinder<UDataTable> DataTable(TEXT("/Game/02_DataTable/DT_LevelUp.DT_LevelUp"));
-	if (DataTable.Succeeded())
-	{
-		LevelUpDataTable = DataTable.Object;
-		UE_LOG(LogTemp, Error, TEXT("LevelUpDataTable == Succeeded"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Error, TEXT("LevelUpDataTable == unllptr"));
-	}
+	//static ConstructorHelpers::FObjectFinder<UDataTable> DataTable(TEXT("/Game/02_DataTable/DT_LevelUp.DT_LevelUp"));
+	//if (DataTable.Succeeded())
+	//{
+	//	LevelUpDataTable = DataTable.Object;
+	//	UE_LOG(LogTemp, Error, TEXT("LevelUpDataTable == Succeeded"));
+	//}
+	//else
+	//{
+	//	UE_LOG(LogTemp, Error, TEXT("LevelUpDataTable == unllptr"));
+	//}
 }
 
 void ALOGPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Error, TEXT("BeginPlay"));
-
-	if (LevelUpDataTable != nullptr)
-	{
-		for (int32 i = 1; i <= 10; i++)
-		{
-			FLevelUpTableRow* LevelUpTableRow = LevelUpDataTable->FindRow<FLevelUpTableRow>(FName(*(FString::FormatAsNumber(i))), FString(""));
-			UE_LOG(LogTemp, Log, TEXT("Lv.%d :: ExpToNextLevel(%d) TotalExp(%d)"), i, (*LevelUpTableRow).ExpToNextLevel, (*LevelUpTableRow).TotalEXP);
-		}
-	}
+	//UE_LOG(LogTemp, Error, TEXT("BeginPlay"));
+	//
+	//if (LevelUpDataTable != nullptr)
+	//{
+	//	for (int32 i = 1; i <= 10; i++)
+	//	{
+	//		FLevelUpTableRow* LevelUpTableRow = LevelUpDataTable->FindRow<FLevelUpTableRow>(FName(*(FString::FormatAsNumber(i))), FString(""));
+	//		UE_LOG(LogTemp, Log, TEXT("Lv.%d :: ExpToNextLevel(%d) TotalExp(%d)"), i, (*LevelUpTableRow).ExpToNextLevel, (*LevelUpTableRow).TotalEXP);
+	//	}
+	//}
 }
